88e92abbf0992b1a700fd17ae14f43a031233a58
AnGon777 <ikhtiaj.arif@gmail.com>
2023-04-04 00:46:20 +0600

    signup with JWT implemented and tested

--- index.js
+++ index.js
@@ -31,8 +31,50 @@ function verifyJWT(req, res, next){
     })
 }
 
+//mongodb credentials
+const uri = process.env.DB_URI;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
+async function run() {
+    try{
+        const usersCollection = client.db('todo').collection('users')
+        const todosCollection = client.db('todo').collection('todos')
+
+
+        app.get('/users', async(req, res) => {
+            const query = {role: null};
+            const result = await usersCollection.find(query).toArray();
+            res.send(result)
+        })
+
+// set or update user to database  
+app.put('/users/:email', async(req, res)=>{
+    const user = req.body;
+    const email = req.params.email;
+    const filter = { email: email };
+    const option = { upsert: true};
+    const updatedDoc = {
+        $set: user 
+    }
+    const result = await usersCollection.updateOne(filter, updatedDoc, option);
+
+    const token = jwt.sign(user, process.env.TOKEN_SECRET, {expiresIn: '1d'});
+    console.log(result);
+    res.send({user, token})
+})
+// get all
+
+
+
+
+
+
+    } finally{
+
+}
+}
+run().catch(e => console.dir(e))
 
 
 
